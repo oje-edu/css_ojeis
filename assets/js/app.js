0ddb86a37866e74f86d494092b369a5e3fe5ee35
oje-edu <thorsten.oje@gfn.education>
2021-08-30 01:09:52 +0200

    mobile doesnt fit yet

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,11 @@
+const innertext = document.querySelector(".circle-logo p");
+innertext.innerHTML = innertext.innerText
+  .split("")
+  .map(
+    (char, i) => `<span style="transform:rotate(${i * 10.8}deg)">${char}</span>`
+  )
+  .join("");
+
 const text = document.querySelector(".circle-text p");
 text.innerHTML = text.innerText
   .split("")
