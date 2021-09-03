1ba67c90f6e53d4c07febd9193b7e9879a5f3719
oje-edu <thorsten.oje@gfn.education>
2021-09-03 21:01:41 +0200

    added easter egg - not finished yet

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,16 @@ function toggleBox() {
   var element = document.querySelector(".toggle");
   element.style.display = element.style.display == "flex" ? "none" : "flex";
 }
+function toggleBall() {
+  var ball = document.querySelector(".toggle-ball");
+  ball.style.display = ball.style.display == "flex" ? "none" : "flex";
+  toggleBox();
+}
+function toggleCanvas1() {
+  var canvas1 = document.querySelector(".toggle-canvas1");
+  canvas1.style.display = canvas1.style.display == "flex" ? "none" : "flex";
+  toggleBox();
+}
 
 const innertext = document.querySelector(".circle-logo p");
 innertext.innerHTML = innertext.innerText
